ce3688ba127f8dd4ad3ff30d368cbc70c08891f8
James White <james@jfwhite.org>
2021-01-13 18:56:53 -0800

    add a new template to better style account center output

---
+++ page-templates/account_template.php
@@ -0,0 +1,61 @@
+<?php
+/* Template name: Account Template Page */
+get_header();
+$pageID = get_the_id();
+
+get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area account-managment">
+		<main id="main" class="site-main" role="main">
+
+		<section class="log-in-alert not-logged-in">
+			<p>Thank you for subscribing. <a href="<?php echo home_url('/issues/'); ?>">Read our latest issue here</a>.</p>
+		</section>
+
+			<?php
+            while (have_posts()) :
+                the_post();
+
+                get_template_part('template-parts/page/content', 'page');
+
+                // If comments are open or we have at least one comment, load up the comment template.
+                if (comments_open() || get_comments_number()) :
+                    comments_template();
+                endif;
+
+            endwhile; // End the loop.
+            ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<style type="text/css">
+
+	.log-in-alert {
+		max-width: 480px;
+		margin-left: auto;
+		margin-right: auto;
+
+	}
+
+	.log-in-alert .not-logged-in {
+		display: none;
+	}
+</style>
+
+<script type="text/javascript"> 
+ 
+	jQuery(document).ready(function(){ 
+
+		if( ('#wpfs-manage-subscriptions-container').length > 1 ) {
+			jQuery('.log-in-alert').toggleClass('not-logged-in');
+		}
+	});
+
+
+</script>
+
+<?php
+get_footer();
