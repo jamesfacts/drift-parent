fb9e78dc9b25e041d97dbe466903c722f4bc554f
James White <james@jfwhite.org>
2022-01-25 09:48:50 -0800

    add post id on latest

--- page-templates/latest_articles.php
+++ page-templates/latest_articles.php
@@ -22,30 +22,30 @@ $wp_query = null; // re-sets query
 ?>
 <?php  $args =  $args = array( 'posts_per_page' => 10, 'paged'=> $paged, 'order' => 'DESC',  'post_type'=>'post','order' => 'DESC' );
 				$wp_query = new WP_Query();
-				$wp_query->query( $args ); 
-				$i=1;			
+				$wp_query->query( $args );
+				$i=1;
 				?>
 
-<?php  while($wp_query->have_posts()) :    $wp_query->the_post();  
-	
+<?php  while($wp_query->have_posts()) :    $wp_query->the_post();
+
 	  	$postID = get_the_id();
 	  	$thumbID = get_post_thumbnail_id($postID);
-	  	
+
 	  	if($thumbID != "")
 	  	{
-		  	$thumbURL = wp_get_attachment_image_src($thumbID, "full");	  	
+		  	$thumbURL = wp_get_attachment_image_src($thumbID, "full");
 		  	$thumbURL = $thumbURL[0];
 	  	}
 	?>
 	<div class="term-list">
 		<div class="row">
 			<div class="col-md-6 list-feature-img">
-			<?php 
+			<?php
 			  if($thumbID != "")
 			  {
 			  	?>
 			  	<a href="<?php echo get_the_permalink($postID); ?>">
-				  <img src="<?php echo $thumbURL;?>" alt="">			
+				  <img src="<?php echo $thumbURL;?>" alt="">
 				</a>
 			  	<?php
 			  }
@@ -53,14 +53,14 @@ $wp_query = null; // re-sets query
 			  {
 			  	?>
 			  	<a href="<?php echo get_the_permalink($postID); ?>">
-			  	  <img src="<?php echo get_bloginfo("template_url");?>/assets/images/unnamed.jpg" alt="">			
+			  	  <img src="<?php echo get_bloginfo("template_url");?>/assets/images/unnamed.jpg" alt="">
 			  	 </a>
 			  	<?php
 			  }
 			?>
 			</div>
-			<div class="col-md-6 list-feature-info">
-				<?php 
+			<div class="col-md-6 list-feature-info <?php echo "postid-" . $postID ?>">
+				<?php
 					$pageID = get_the_id();
 					$pageTitle = get_the_title();
 					$pagePermalink = get_the_permalink();
@@ -80,22 +80,22 @@ $wp_query = null; // re-sets query
 				<h4 class="article_date"><?php echo $articleDate; ?></h4>
 				<h2>
 					<a href="<?php echo $pagePermalink; ?>"><?php echo $pageTitle;?></a>
-				<?php 
+				<?php
 					if($subsitle != "")
 					{
 						if(in_array($currenatPostID, $featuredPosts))
 						    {
-					          echo "<span style='".$colorPick."'> | </span> <a href='".$pagePermalink."'>".$subsitle."</a>";	    	
+					          echo "<span style='".$colorPick."'> | </span> <a href='".$pagePermalink."'>".$subsitle."</a>";
 						    }
 						    else
 						    {
-                              echo "<span> | </span> <a href='".$pagePermalink."'>".$subsitle."</a>";	
+                              echo "<span> | </span> <a href='".$pagePermalink."'>".$subsitle."</a>";
 						    }
-	                   
+
 					}
 			    ?>
 			    </h2>
-				 
+
 				<h3>
 							<?php
 							 $post_authors = get_the_terms( $pageID, 'authors' );
@@ -107,12 +107,12 @@ $wp_query = null; // re-sets query
 					 			 {
 					 			 	$loopNum++;
 					 			 	$author_id = $post_author->term_id;
-					 			 	
+
 					 			 	$author_link = get_term_link($post_author);
 					 			 	$author_name = $post_author->name;
 					 			 	$author_description = $post_author->description;
-									 	 			 
-					 			 	
+
+
 					 			 	if($loopNum == 1)
 					 			 	{
 					 			 		?><a href="<?php echo $author_link; ?>"><?php echo $author_name;?></a><?php
@@ -127,7 +127,7 @@ $wp_query = null; // re-sets query
 							/* translators: Search query. */
 				                $authorName = single_term_title();
 							echo $tresty = get_query_var( 'author' );
-							
+
 							printf( __( $authorName, 'twentyseventeen' ), '<span>' . get_search_query() . '</span>' );
 							?>
 			</h3>
@@ -137,13 +137,13 @@ $wp_query = null; // re-sets query
 	</div>
 	<?php $i++; endwhile;  ?>
   <div class="page_navigation">
-       		<?php 	
+       		<?php
 					wp_pagenavi( array('query'=>$wp_query)) ;
-					wp_reset_postdata();		
-	         ?>                
+					wp_reset_postdata();
+	         ?>
              </div>
 
-	
+
 </div>
 </div>
 
