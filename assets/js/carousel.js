0fc5259dfe8008d0f7bc7533073cca79b8f77138
Thayer Anderson <tanderson11@gmail.com>
2023-06-22 08:21:27 -0400

    Chevrons for carousel. Better permalinks for issues.

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -3,4 +3,6 @@ jQuery('.owl-carousel').owlCarousel({
    lazyLoad:true,
    items:4,
    margin:20,
+   nav:true,
+   navText : ['<img src="' + template_url + '/assets/images/angle-left.png">','<img src="' + template_url + '/assets/images/angle-right.png">'],
  });
