a16108074967e9301badbb006a37841559abfac9
Thayer Anderson <tanderson11@gmail.com>
2023-07-03 14:56:38 -0400

    Dupe of last.

--- page-templates/mixed_subscribe.php
+++ page-templates/mixed_subscribe.php
@@ -1,5 +1,7 @@
 <?php
 /* Template name: Mixed subscribe page */
+$meta_bundle = get_post_meta( $bundle_id, 'promoted-bundle', true );
+print_r($meta_bundle);
 get_header();
 ?>
 
@@ -23,8 +25,6 @@ endif;
 </div>
 <?php
 // Query BOMB bundle subscription page for first section
-$bundle_id = get_post_meta( $bundle_id, 'promoted-bundle', true );
-echo($bundle_id);
 $bundle_id = get_page_by_path("/bomb-x-the-drift");
 $bundle_content = $bundle_id->post_content;
 $bundle_subsitle = get_post_meta( $bundle_id, 'subsitle', true );
