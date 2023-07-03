e9663ce53fb76f7f2db02b767ccc4b1132c6746f
Thayer Anderson <tanderson11@gmail.com>
2023-07-03 19:42:22 -0400

    Trying lower js.

--- page-templates/bundle_subscribe.php
+++ page-templates/bundle_subscribe.php
@@ -24,12 +24,6 @@ if ( $image = get_the_post_thumbnail_url( get_the_ID(), 'full' ) ) :
 		}
 	</style>
 
-	<script type="text/javascript">
-		const button = document.getElementById('wpfs-billing-address-country--<?php echo($formID);?>-button');
-		button.setAttribute('disabled', 'disabled');
-		button.setAttribute('aria-disabled', 'disabled');
-	</script>
-
 	<?php
 endif;
 ?>
@@ -79,6 +73,12 @@ endif;
 	</div>
 </div>
 </section>
+
+<script type="text/javascript">
+		const button = document.getElementById('wpfs-billing-address-country--<?php echo($formID);?>-button');
+		button.setAttribute('disabled', 'disabled');
+		button.setAttribute('aria-disabled', 'disabled');
+</script>
 
 <?php
 get_footer();
